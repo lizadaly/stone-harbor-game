8a61f9eb74c8d8d0ce96429c32aa61f8b05d2b09
Liza Daly <lizadaly@gmail.com>
2016-09-10 16:22:31 -0400

    Register deployment with opswhatever

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 import os
 
-from fabric.api import local, settings, abort, run, cd, env, put
+from fabric.api import local, settings, abort, run, cd, env, put, runs_once, lcd, local, task
 from fabric.contrib.console import confirm
 
 import time
@@ -11,14 +11,17 @@ env.user = 'django'
 env.hosts = ['162.243.253.184']
 deploy_dir = 'deploy'
 
-
+@task
 def deploy():
     code_dir = '/home/django/apps/games/stone-harbor'
     deploy_app(code_dir)
+#    register_deployment('.')
 
+@task
 def deploy_qa():
     code_dir = '/home/django/apps/games/staging/stone-harbor'
     deploy_app(code_dir)
+    register_deployment('.')
 
 def deploy_app(code_dir):
 
@@ -37,3 +40,16 @@ def deploy_app(code_dir):
                                       code_dir + '/' + filename))
     run('cd {} && rm {}'.format(code_dir,
                                 code_dir + '/' + filename))
+
+
+@task
+@runs_once
+def register_deployment(git_path):
+    with(lcd(git_path)):
+        revision = local('git log -n 1 --pretty="format:%H"', capture=True)
+        branch = local('git rev-parse --abbrev-ref HEAD', capture=True)
+        local('curl https://intake.opbeat.com/api/v1/organizations/20455e2f554647e7a3e723eacca96171/apps/b62baee357/releases/'
+              ' -H "Authorization: Bearer 4d48bec505b4ea6f6a5f1b44c5b15b9940e0c205"'
+              ' -d rev="{}"'
+              ' -d branch="{}"'
+              ' -d status=completed'.format(revision, branch))
