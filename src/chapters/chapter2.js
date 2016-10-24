d680066bdef908fcbae09f89bd58da93b3cdb6ec
Liza Daly <lizadaly@users.noreply.github.com>
2016-10-23 20:29:08 -0400

    Smart quote fix

--- src/chapters/chapter2.js
+++ src/chapters/chapter2.js
@@ -37,7 +37,7 @@ const _Chapter = ({currentSection, inventory, chapterId}) => {
         she barks. “Just tell me where it is.”
       </p>
       <p>
-        “Don't want me planting any evidence?”
+        “Don’t want me planting any evidence?”
       </p>
       <p>
         “Exactly.”
