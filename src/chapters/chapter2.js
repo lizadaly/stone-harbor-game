0b8ff5a8c5fba45a3292292108b4f4df411c5d4f
Liza Daly <lizadaly@users.noreply.github.com>
2016-10-23 20:30:04 -0400

    Remove extraneous quote in accessible text

--- src/chapters/chapter2.js
+++ src/chapters/chapter2.js
@@ -104,7 +104,7 @@ const _Chapter = ({currentSection, inventory, chapterId}) => {
       </section>,
       <div className="in-flashback">
         <figure className="chapter2">
-          <figcaption className="visuallyhidden">An old-fashioned parlor, with overstuffed chairs and a vase of wilted flowers, in black and white"</figcaption>
+          <figcaption className="visuallyhidden">An old-fashioned parlor, with overstuffed chairs and a vase of wilted flowers, in black and white</figcaption>
         </figure>
         <h3>In the family room</h3>
         <section>
