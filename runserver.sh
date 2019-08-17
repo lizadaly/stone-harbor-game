d6f9e280194067a014a7ad1d3e1ede4a30add79b
Liza Daly <lizadaly@gmail.com>
2019-08-17 18:08:07 -0400

    Correct various loader problems

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Start the local webserver, available on port 8000
-python3 -m http.server --cgi 8000
+python3 -m http.server --cgi 8000 &
 
 # If you have only Python 2, comment out the previous line and
 # replace with:
 # python -m SimpleHTTPServer 8000
 
 # Watch for changes to the build
-webpack -w &
+node_modules/webpack/bin/webpack.js -w &
