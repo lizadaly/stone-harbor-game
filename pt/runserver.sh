6c8b0495a86646f899300f8ce1a76bea02c3fa8d
Liza Daly <lizadaly@gmail.com>
2023-07-04 10:43:14 -0400

    Add build steps for PT

--- pt/runserver.sh
+++ pt/runserver.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start the local webserver, available on port 8001
-python3.9 -m http.server --cgi 8001 &
+python3 -m http.server --cgi 8001 &
 
 # If you have only Python 2, comment out the previous line and
 # replace with:
